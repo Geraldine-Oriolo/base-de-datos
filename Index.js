cd7578ef3e052712532c8e1370725acd8fdef2b9
Geraldine-Oriolo <49374998@est.ort.edu.ar>
2025-05-22 10:30:21 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -10,13 +10,13 @@ let alumno =
 console.log(alumno);
 
 let alumnos = [
-    {nombre: "Juan", apellido: "Gomez", dni: 46345678, anio: 4, curso: "A", nota: 8,},
+    {nombre: "Juan", apellido: "Gomez", dni: 48345678, anio: 4, curso: "A", nota: 8,},
     {nombre: "Meli", apellido: "Kenigsberg", dni: 48860325, anio: 4, curso: "A", nota: 10,},
-    {nombre: "Geraldine", apellido: "Oriolo", dni: 49374998, anio: 5, curso: "A", nota: 9,},
-    {nombre: "Maite", apellido: "Galarza", dni: 49189070, anio: 5, curso: "A", nota: 3,},
+    {nombre: "Geraldine", apellido: "Oriolo", dni: 48374998, anio: 5, curso: "A", nota: 9,},
+    {nombre: "Maite", apellido: "Galarza", dni: 48189070, anio: 5, curso: "A", nota: 3,},
     {nombre: "Maia", apellido: "Kisinovsky", dni: 48187071, anio: 4, curso: "B", nota: 5,},
-    {nombre: "Mia", apellido: "German", dni: 49989070, anio: 4, curso: "B", nota: 4,},
-    {nombre: "Mia", apellido: "Cuesta", dni: 49169007, anio: 3, curso: "B", nota: 4,},
+    {nombre: "Mia", apellido: "German", dni: 48989070, anio: 4, curso: "B", nota: 4,},
+    {nombre: "Mia", apellido: "Cuesta", dni: 48169007, anio: 3, curso: "B", nota: 4,},
     {nombre: "Agustina", apellido: "Romano", dni: 48375887, anio: 3, curso: "B", nota: 9,},
 
     ]
